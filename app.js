2edac19d4f68d7bcb10aafa3ca3d23c354cc86b5
divineLush <alvipath@yahoo.com>
2021-09-17 18:51:10 +0700

    404 page

--- app.js
+++ app.js
@@ -14,5 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(adminRoutes)
 app.use(shopRoutes)
 
+// 404 page
+app.use((req, res, next) => {
+    res.status(404).send('<h1>not found</h1>')
+})
+
 const server = http.createServer(app)
 server.listen(3000)
